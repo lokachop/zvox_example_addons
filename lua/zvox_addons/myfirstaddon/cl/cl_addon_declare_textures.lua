08670d59406db52af28be3cda9cc6caf1f76e6aa
Lokachop <69372103+lokachop@users.noreply.github.com>
2025-11-05 16:05:58 +0100

    update this to tex support =D

--- lua/zvox_addons/myfirstaddon/cl/cl_addon_declare_textures.lua
+++ lua/zvox_addons/myfirstaddon/cl/cl_addon_declare_textures.lua
@@ -2,7 +2,7 @@ local ADDON = ZVox.AddonAPI.GetCurrentAddon()
 
 ZVox.AddonAPI.AddAddonHook(ADDON, "DeclareTextures", function()
 	ZVox.AddonAPI.NewTexture("my_texture", function()
-		render.Clear(255, 0, 0)
+		render.Clear(255, 0, 0, 255)
 	end)
 
 	ZVox.AddonAPI.TextureOpPixelFunc("my_texture", function(x, y)
@@ -12,4 +12,35 @@ ZVox.AddonAPI.AddAddonHook(ADDON, "DeclareTextures", function()
 	ZVox.AddonAPI.NewTexturePixelFunc("my_texture_two", function(x, y)
 		return 0, x * 16, y * 16
 	end)
+
+
+	ZVox.NewTextureAnimated("my_animated_texture", {
+		["frames"] = 16,
+		["speed"] = 0.1,
+	}, function(name, delta, frame)
+		ZVox.TextureOpPixelFunc(name, function(x, y)
+			--local xc = (x + (delta * 16)) % 16
+			--local yc = (y + (delta * 32)) % 16
+
+			local delta16 = delta * 16
+
+			local xc = x - delta16
+			local yc = y
+
+			local val1 = (bit.bxor(math.floor(xc), math.floor(yc)) * 16) % 256
+
+			local xc = x + delta16
+			local yc = y
+			local val2 = (bit.bxor(math.floor(xc), math.floor(yc)) * 16) % 256
+
+			local xc = x
+			local yc = y - delta16
+			local val3 = (bit.bxor(math.floor(xc), math.floor(yc)) * 16) % 256
+
+
+			return val1, val2, val3
+		end)
+	end)
+
+	ZVox.AddonAPI.SetTextureEmissive("my_animated_texture", true)
 end)
--- lua/zvox_addons/myfirstaddon/sh/sh_addon_declare_voxels.lua
+++ lua/zvox_addons/myfirstaddon/sh/sh_addon_declare_voxels.lua
@@ -8,18 +8,11 @@ ZVox.AddonAPI.AddAddonHook(ADDON, "DeclareVoxels", function()
 		["name"] = "my_voxel",
 		["visible"] = true,
 		["solid"] = true,
-		["multitex"] = {
-			[ZVOX_TEX_X_PLUS ] = "zvox:xor", -- +X
-			[ZVOX_TEX_X_MINUS] = "zvox:xor", -- -X
-			[ZVOX_TEX_Y_PLUS ] = "zvox:xor", -- +Y
-			[ZVOX_TEX_Y_MINUS] = "zvox:xor", -- -Y
-			[ZVOX_TEX_Z_PLUS ] = "zvox:xor", -- +Z
-			[ZVOX_TEX_Z_MINUS] = "zvox:xor", -- -Z
-		},
-		["tex"] = "zvox:xor",
+		["tex"] = ZVox.AddonAPI.NamespaceConvert("my_animated_texture"),
 		["voxelgroup"] = ZVOX_VOXELGROUP_SOLID,
 		["sound"] = ZVOX_MAT_METAL,
 		["voxelmodel"] = "zvox:cube_dir",
+		["emmissive"] = 15,
 	})
 
 	ZVox.AddonAPI.NewVoxel({
@@ -27,9 +20,9 @@ ZVox.AddonAPI.AddAddonHook(ADDON, "DeclareVoxels", function()
 		["visible"] = true,
 		["solid"] = true,
 		["opaque"] = false,
-		["tex"] = "zvox:voidinium_wool",
+		["tex"] = ZVox.AddonAPI.NamespaceConvert("my_texture"), -- ZVox.AddonAPI.NamespaceConvert will add our namespace to the string
 		["voxelgroup"] = ZVOX_VOXELGROUP_SOLID,
 		["sound"] = ZVOX_MAT_CLOTH,
-		["voxelmodel"] = "myfirstaddon:evil_model_two_the_return_of_evil",
+		["voxelmodel"] = ZVox.AddonAPI.NamespaceConvert("evil_model_two_the_return_of_evil"),
 	})
 end)
